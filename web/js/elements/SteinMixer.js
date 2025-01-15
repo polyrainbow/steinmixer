b58e700b8f635b9e75ce7915d4cced136c999ff4
Sebastian <5648131+polyrainbow@users.noreply.github.com>
2025-01-15 05:00:00 +0000

    feat: layout update

--- web/js/elements/SteinMixer.js
+++ web/js/elements/SteinMixer.js
@@ -240,25 +240,25 @@ customElements.define("stein-mixer", class SteinMixer extends HTMLElement {
 
     const template = html`
     <app-header connection-name=${this.connectionName}></app-header>
+    <div class="mix-select">
+      <button
+        class=${mix === 0 ? "active" : ""}
+        @click=${() => {
+          this.device.selectActiveMix(0);
+          this.activeMix = 0;
+          this.render();
+        }}
+      >Mix 1</button>
+      <button
+        class=${mix === 1 ? "active" : ""}
+        @click=${() => {
+          this.device.selectActiveMix(1);
+          this.activeMix = 1;
+          this.render();
+        }}
+      >Mix 2</button>
+    </div>
     <div class="mixer-container">
-      <div class="mix-select">
-        <button
-          class=${mix === 0 ? "active" : ""}
-          @click=${() => {
-            this.device.selectActiveMix(0);
-            this.activeMix = 0;
-            this.render();
-          }}
-        >Mix 1</button>
-        <button
-          class=${mix === 1 ? "active" : ""}
-          @click=${() => {
-            this.device.selectActiveMix(1);
-            this.activeMix = 1;
-            this.render();
-          }}
-        >Mix 2</button>
-      </div>
       <stereo-input
         .device=${this.device}
         link-inputs=${params.Input0Link}
@@ -361,82 +361,16 @@ customElements.define("stein-mixer", class SteinMixer extends HTMLElement {
           this.render();
         }}
       ></channel-strip>
-      <master-reverb
-        .device=${this.device}
-        active-mix=${this.activeMix}
-      ></master-reverb>
     </div>
 
-    <div class="settings">
-      <div class="setting">
-        <label class="select-label" for="phones-2-output-select">
-          Phones 2 output
-        </label>
-        <select id="phones-2-output-select"
-          @change=${(e) => {
-            this.params.Phones2Output = parseInt(e.target.value);
-            this.device.updateParamValue(
-              "Phones2Output",
-              parseInt(e.target.value),
-            );
-          }}
-          .value=${live(params.Phones2Output)}
-        >
-          <option value="0">Mix 1</option>
-          <option value="1">Mix 2</option>
-        </select>
-      </div>
-      <div class="setting">
-        <label class="select-label" for="input-56-level-select">
-          Input 5/6 level
-        </label>
-        <select id="input-56-level-select"
-          @change=${(e) => {
-            this.device.updateParamValue(
-              "Input45Level",
-              parseInt(e.target.value),
-            );
-          }}
-          .value=${live(params.Input45Level)}
-        >
-          <option value="1">-10dBV</option>
-          <option value="0">+4dBu</option>
-        </select>
-      </div>
-      <div class="setting">
-        <label class="select-label" for="hpf-select">
-          HPF
-        </label>
-        <select
-          id="hpf-level-select"
-          @change=${(e) => {
-            const value = parseInt(e.target.value);
-            this.device.updateParamValue("InputHPFSetting", value);
-          }}
-          .value=${live(params.InputHPFSetting)}
-        >
-          <option value="0">40 Hz</option>
-          <option value="1">60 Hz</option>
-          <option value="2">80 Hz</option>
-          <option value="3">100 Hz</option>
-          <option value="4">120 Hz</option>
-        </select>
-      </div>
-      <div class="setting">
-        <label>
-          <input
-            type="checkbox"
-            .checked=${live(this.params.Loopback === 1)}
-            @change=${(e) => {
-              this.device.updateParamValue(
-                "Loopback",
-                e.target.checked ? 1 : 0,
-              );
-            }}
-          >
-          Loopback of DAW signal to Inputs 1/2
-      </div>
-    </div>
+    <master-reverb
+      .device=${this.device}
+      active-mix=${this.activeMix}
+    ></master-reverb>
+
+    <interface-settings
+      .device=${this.device}
+    ></interface-settings>
     `;
 
     render(template, this);
